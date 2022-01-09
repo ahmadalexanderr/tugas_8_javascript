c09c3f5ada6353e1e2f848842703ced4137f396c
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-09 11:19:04 +0700

    Upload Tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+const cigaratte = () => {
+    var rokok = {
+        merk : 'Lucky Strike',
+        harga: 28000
+    }
+    rokok.harga = 27500;
+    rokok.rasa = ['mild', 'mint', 'menthol'];
+    console.log(rokok);
+}
+
+cigaratte();
